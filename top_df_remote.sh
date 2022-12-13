c6b5bdfc85e7f7c38957a6930dfe71704023fa6e
Pawoj3 <119871219+Pawoj3@users.noreply.github.com>
2022-12-13 10:13:51 +0100

    Update top_df_remote.sh

--- top_df_remote.sh
+++ top_df_remote.sh
@@ -6,5 +6,5 @@ if [ "$res" == 1 ];then
     echo "Target $TARGET is not reachable"
 else
     scp  "$ftr" $TARGET:
-    ssh $TARGET './$ftr; source top_df_local.sh; scp "./$folder_name/$file_name" 192.168.56.102:./code/'
+    ssh $TARGET './$ftr; source top_df_local.sh; scp "./$now" 192.168.56.102:./code/'
 fi
