a5114f5a12d8b196c49e2f01ecde3848748e5f3e
Pawoj3 <119871219+Pawoj3@users.noreply.github.com>
2022-12-13 10:09:11 +0100

    Update top_df_remote.sh

--- top_df_remote.sh
+++ top_df_remote.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env bash
-
-scp top_df_local.sh dev@192.168.56.123:top_df_local.sh 
-
-echo "Transfer done"
-
+TARGET=192.168.56.123
+ftr=top_df_local.sh
+ping $TARGET -c1 -t1 > /dev/null 2>&1
+res=$?
+if [ "$res" == 1 ];then
+    echo "Target $TARGET is not reachable"
+else
+    scp  "$ftr" $TARGET:
+    ssh $TARGET './$ftr; source top_df_local.sh; scp "./$folder_name/$file_name" 192.168.56.102:./code/'
+fi
